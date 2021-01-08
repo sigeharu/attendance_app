33fb0cc19edf210ef1bb64ab630e2c2af8485df4
sigeharu <sigeharumiyamoto@gmail.com>
2021-01-08 22:25:42 +0900

    Finish add-user-attendances

--- db/schema.rb
+++ db/schema.rb
@@ -2,15 +2,26 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210107025950) do
+ActiveRecord::Schema.define(version: 2021_01_08_091624) do
+
+  create_table "attendances", force: :cascade do |t|
+    t.date "worked_on"
+    t.datetime "started_at"
+    t.datetime "finished_at"
+    t.string "note"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_attendances_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -21,9 +32,10 @@ ActiveRecord::Schema.define(version: 20210107025950) do
     t.string "remember_digest"
     t.boolean "admin", default: false
     t.string "department"
-    t.datetime "basic_time", default: "2021-01-06 23:00:00"
-    t.datetime "work_time", default: "2021-01-06 22:30:00"
+    t.datetime "basic_time", default: "2021-01-07 23:00:00"
+    t.datetime "work_time", default: "2021-01-07 22:30:00"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "attendances", "users"
 end
