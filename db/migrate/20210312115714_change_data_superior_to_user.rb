ca2f29c36bfe7724d37b6e125907b56d7e8a72d8
miyamoto sigeharu <sigeharumiyamoto@gmail.com>
2021-04-12 06:30:41 +0900

    heroku deploy4

--- db/migrate/20210312115714_change_data_superior_to_user.rb
+++ db/migrate/20210312115714_change_data_superior_to_user.rb
@@ -1,5 +1,5 @@
 class ChangeDataSuperiorToUser < ActiveRecord::Migration[6.0]
   def change
-    change_column :users, :superior, 'integer USING CAST(superior AS integer)', default: false
+    change_column :users, :superior, "USING superior::integer", default: false
   end
 end
